0d77f23c8c033f7f9698aba11d17ef4fe0eb59f1
GhostDuckyy <95024755+GhostDuckyy@users.noreply.github.com>
2021-12-05 13:01:31 +0800

    Update Kavo Ui library[Edited].lua

--- Kavo/Kavo Ui library[Edited].lua
+++ Kavo/Kavo Ui library[Edited].lua
@@ -118,8 +118,16 @@ local themeStyles = {
         Header = Color3.fromRGB(22, 29, 31),
         TextColor = Color3.fromRGB(255,255,255),
         ElementColor = Color3.fromRGB(22, 29, 31)
-    }
+    },
+   -- Made by Myself!
+   Discord = {
+    SchemeColor = Color3.fromRGB(92,100,244),
+    Background = Color3.fromRGB(54,57,63),
+    Header = Color3.fromRGB(47,49,54),
+    TextColor = Color3.fromRGB(210,221,212),
+    ElementColor = Color3.fromRGB(47,49,54)
 }
+
 local oldTheme = ""
 
 local SettingsT = {
@@ -275,7 +283,7 @@ function Kavo.CreateLib(kavName, themeList)
     close.Position = UDim2.new(0.949999988, 0, 0.137999997, 0)
     close.Size = UDim2.new(0, 21, 0, 21)
     close.ZIndex = 1
-    close.Image = "http://www.roblox.com/asset/?id=8161918504"
+    close.Image = "http://www.roblox.com/asset/?id=8167926955"
     close.ImageRectOffset = Vector2.new(0,0)
     close.ImageRectSize = Vector2.new(0,0)
     close.MouseButton1Click:Connect(function()
