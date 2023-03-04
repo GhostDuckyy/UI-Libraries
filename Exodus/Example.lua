d3002f86b91b9445ee95bd5c00ff4ca23a5c89d9
Duckyy <95024755+GhostDuckyy@users.noreply.github.com>
2023-03-04 15:59:23 +0800

    Create Example.lua

---
+++ Exodus/Example.lua
@@ -0,0 +1,94 @@
+    local library = loadstring(game:HttpGet("https://github.com/WetCheezit/UI-Libs/blob/main/Exodus.lua?raw=true"))()
+    local window = library:Load{playerlist = true}
+    library.playerlist:button{name = "Prioritize", callback = function(list, plr)
+        if not list:IsTagged(plr, "Prioritized") then
+            list:Tag{player = plr, text = "Prioritized", color = fromRGB(255, 0, 0)}
+        else
+            list:RemoveTag(plr, "Prioritized")
+        end
+    end}
+    library.playerlist:button{name = "Ignore", callback = function(list, plr)
+        if not library.playerlist:IsTagged(plr, "Ignored") then
+            library.playerlist:Tag{player = plr, text = "Ignored", Color = fromRGB(120, 120, 120)}
+        else
+            library.playerlist:RemoveTag(plr, "Ignored")
+        end
+    end}
+    library.playerlist:Label{name = "Rank: ", handler = function(plr)
+        return "1e+9"
+    end}
+    library.playerlist:Label{name = "Team: ", handler = function(plr)
+        return "Ghosts", fromRGB(209, 118, 0)
+    end}
+    local watermark = library:Watermark("exodus | dev | test | 2.3b fps")
+    window:SettingsTab(watermark)
+    local tab = window:Tab("rage")
+    local tab2 = window:Tab("visuals")
+    window:Tab("legit"):Section{
+        Side = "Middle"
+    }
+    tab:Section{}
+    local sec = tab:Section{
+        Side = "Right"
+    }
+    local label = sec:Label("fart")
+    label:Set("hi")
+    sec:Button{}
+    sec:Button{}
+    sec:Button{}
+    sec:Button{}
+    sec:Dropdown{content = {"hi", "bye"}}
+    sec:Button{}
+    local m = sec:Separator()
+    sec:Button{name = "save config", callback = function() library:SaveConfig("fart") end}
+    sec:Button{name = "load config", callback = function() library:LoadConfig("fart", true) end}
+    sec:Button{callback = function() m:Set("testing 123") end}
+    local tog = sec:Toggle{}
+    tog:Slider{}
+    local f = tog:Colorpicker{alpha = 1}
+    f:SetAlpha(0.5)
+    tog:Colorpicker{}
+    local togel = sec:Toggle{}
+    togel:Colorpicker{alpha = 1}
+    togel:Colorpicker{}
+    local togel = sec:Toggle{}
+    togel:Slider{}
+    local fartel = sec:Toggle{name = "aimbot"}
+    fartel:Dropdown{}
+    fartel:Keybind{mode = "hold", listname = "aimbot share fr"}
+    local fartel = sec:Toggle{name = "keybind toggle"}
+    fartel:Keybind{}
+    sec:Box{
+        callback = function(str)
+            print(str)
+        end,
+        clearonfocus = false
+    }
+    sec:Slider{}
+    sec:Keybind{}
+    local label = sec:Label("fart")
+    local enemies, teammates = tab:MultiSection{
+        Side = "Right",
+        Sections = {"enemies", "teammates"}
+    }
+    enemies:Button{}
+    enemies:Button{}
+    teammates:Button{}
+    teammates:Button{}
+    teammates:Button{name = "fart"}
+    teammates:Button{}
+    local label = teammates:Label("fart")
+    label:Set("hi")
+    local tab2 = window:Tab("test")
+    local f = tab2:SubTab("hi")
+    local g = tab2:SubTab("testf")
+    local r = f:Section{name = "fart"}
+    r:Button{}
+    local r = g:Section{name = "poop"}
+    r:Button{}
+    library:Init()
+    --wait(10)
+    --library:Unload()
+    library:Notify{duration = 3, Color = fromRGB(0, 0, 0)}
+    task.wait(1)
+    library:Notify{duration = 3, Color = fromRGB(0, 0, 0)}
