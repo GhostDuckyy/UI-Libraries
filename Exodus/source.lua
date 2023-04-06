d2d2e4eef877d31497ced50748504286b45737db
Duckyy <95024755+GhostDuckyy@users.noreply.github.com>
2023-04-06 21:26:29 +0800

    Update source.lua

--- Exodus/source.lua
+++ Exodus/source.lua
@@ -2860,8 +2860,8 @@ function library:Unload(fade)
         self.watermark:Destroy()
     end
 
-    if (self.playerlist) then
-        self.playerlist.object:Destroy()
+    if (self.Playerlist) then
+        self.Playerlist.object:Destroy()
     end
 
     for list, _ in next, library.lists do
@@ -2884,7 +2884,7 @@ end
 function library:Toggle()
     if not self.opening then
         self.opening = true
-        self.playerlist.object.Visible = self.playerlist.toggled and not self.open or false
+        self.Playerlist.object.Visible = self.Playerlist.toggled and not self.open or false
 
         for window, types in next, library.windows do
             window.Visible = types.Visible and not self.open or false;
@@ -5388,10 +5388,10 @@ function library:Load(options)
             default = library.keybind_list_default,
             flag = "player_list",
             callback = function(value)
-                library.playerlist.toggled = value
+                library.Playerlist.toggled = value
 
                 if library.open then
-                    library.playerlist.object.Visible = value
+                    library.Playerlist.object.Visible = value
                 end
             end
         }
@@ -5519,131 +5519,131 @@ function library:Load(options)
     return window_types
 end
 
---[[
-    local window = library:Load{playerlist = true}
 
-    library.playerlist:button{name = "Prioritize", callback = function(list, plr)
-        if not list:IsTagged(plr, "Prioritized") then
-            list:Tag{player = plr, text = "Prioritized", color = fromRGB(255, 0, 0)}
-        else
-            list:RemoveTag(plr, "Prioritized")
-        end
-    end}
+--[[local window = library:Load{playerlist = true}
 
-    library.playerlist:button{name = "Ignore", callback = function(list, plr)
-        if not library.playerlist:IsTagged(plr, "Ignored") then
-            library.playerlist:Tag{player = plr, text = "Ignored", Color = fromRGB(120, 120, 120)}
-        else
-            library.playerlist:RemoveTag(plr, "Ignored")
-        end
-    end}
+library.Playerlist:button{name = "Prioritize", callback = function(list, plr)
+    if not list:IsTagged(plr, "Prioritized") then
+        list:Tag{player = plr, text = "Prioritized", color = fromRGB(255, 0, 0)}
+    else
+        list:RemoveTag(plr, "Prioritized")
+    end
+end}
 
-    library.playerlist:Label{name = "Rank: ", handler = function(plr)
-        return "1e+9"
-    end}
+library.Playerlist:button{name = "Ignore", callback = function(list, plr)
+    if not library.Playerlist:IsTagged(plr, "Ignored") then
+        library.Playerlist:Tag{player = plr, text = "Ignored", Color = fromRGB(120, 120, 120)}
+    else
+        library.Playerlist:RemoveTag(plr, "Ignored")
+    end
+end}
 
-    library.playerlist:Label{name = "Team: ", handler = function(plr)
-        return "Ghosts", fromRGB(209, 118, 0)
-    end}
+library.Playerlist:Label{name = "Rank: ", handler = function(plr)
+    return "1e+9"
+end}
 
-
-    local watermark = library:Watermark("exodus | dev | test | 2.3b fps")
-    window:SettingsTab(watermark)
-
-    local tab = window:Tab("rage")
-    local tab2 = window:Tab("visuals")
-    window:Tab("legit"):Section{
-        Side = "Middle"
-    }
-
-    tab:Section{}
+library.Playerlist:Label{name = "Team: ", handler = function(plr)
+    return "Ghosts", fromRGB(209, 118, 0)
+end}
 
 
+local watermark = library:Watermark("exodus | dev | test | 2.3b fps")
+window:SettingsTab(watermark)
 
-    local sec = tab:Section{
-        Side = "Right"
-    }
+local tab = window:Tab("rage")
+local tab2 = window:Tab("visuals")
+window:Tab("legit"):Section{
+    Side = "Middle"
+}
 
-    local label = sec:Label("fart")
-    label:Set("hi")
+tab:Section{}
 
-    sec:Button{}
-    sec:Button{}
-    sec:Button{}
-    sec:Button{}
-    sec:Dropdown{content = {"hi", "bye"}}
 
-    sec:Button{}
-    local m = sec:Separator()
-    sec:Button{name = "save config", callback = function() library:SaveConfig("fart") end}
-    sec:Button{name = "load config", callback = function() library:LoadConfig("fart", true) end}
-    sec:Button{callback = function() m:Set("testing 123") end}
 
-    local tog = sec:Toggle{}
-    tog:Slider{}
-    local f = tog:Colorpicker{alpha = 1}
-    f:SetAlpha(0.5)
+local sec = tab:Section{
+    Side = "Right"
+}
 
-    tog:Colorpicker{}
+local label = sec:Label("fart")
+label:Set("hi")
 
-    local togel = sec:Toggle{}
-    togel:Colorpicker{alpha = 1}
-    togel:Colorpicker{}
+sec:Button{}
+sec:Button{}
+sec:Button{}
+sec:Button{}
+sec:Dropdown{content = {"hi", "bye"}}
 
-    local togel = sec:Toggle{}
-    togel:Slider{}
+sec:Button{}
+local m = sec:Separator()
+sec:Button{name = "save config", callback = function() library:SaveConfig("fart") end}
+sec:Button{name = "load config", callback = function() library:LoadConfig("fart", true) end}
+sec:Button{callback = function() m:Set("testing 123") end}
 
-    local fartel = sec:Toggle{name = "aimbot"}
-    fartel:Dropdown{}
-    fartel:Keybind{mode = "hold", listname = "aimbot share fr"}
+local tog = sec:Toggle{}
+tog:Slider{}
+local f = tog:Colorpicker{alpha = 1}
+f:SetAlpha(0.5)
 
-    local fartel = sec:Toggle{name = "keybind toggle"}
-    fartel:Keybind{}
+tog:Colorpicker{}
 
-    sec:Box{
-        callback = function(str)
-            print(str)
-        end,
-        clearonfocus = false
-    }
+local togel = sec:Toggle{}
+togel:Colorpicker{alpha = 1}
+togel:Colorpicker{}
 
-    sec:Slider{}
-    sec:Keybind{}
-    local label = sec:Label("fart")
+local togel = sec:Toggle{}
+togel:Slider{}
 
-    local enemies, teammates = tab:MultiSection{
-        Side = "Right",
-        Sections = {"enemies", "teammates"}
-    }
+local fartel = sec:Toggle{name = "aimbot"}
+fartel:Dropdown{}
+fartel:Keybind{mode = "hold", listname = "aimbot share fr"}
 
-    enemies:Button{}
-    enemies:Button{}
+local fartel = sec:Toggle{name = "keybind toggle"}
+fartel:Keybind{}
 
-    teammates:Button{}
-    teammates:Button{}
-    teammates:Button{name = "fart"}
-    teammates:Button{}
+sec:Box{
+    callback = function(str)
+        print(str)
+    end,
+    clearonfocus = false
+}
 
-    local label = teammates:Label("fart")
-    label:Set("hi")
+sec:Slider{}
+sec:Keybind{}
+local label = sec:Label("fart")
 
-    local tab2 = window:Tab("test")
-    local f = tab2:SubTab("hi")
-    local g = tab2:SubTab("testf")
-    local r = f:Section{name = "fart"}
-    r:Button{}
+local enemies, teammates = tab:MultiSection{
+    Side = "Right",
+    Sections = {"enemies", "teammates"}
+}
 
-    local r = g:Section{name = "poop"}
-    r:Button{}
+enemies:Button{}
+enemies:Button{}
 
-    library:Init()
-    --wait(10)
-    --library:Unload()
-    library:Notify{duration = 3, Color = fromRGB(0, 0, 0)}
-    task.wait(1)
+teammates:Button{}
+teammates:Button{}
+teammates:Button{name = "fart"}
+teammates:Button{}
 
-    library:Notify{duration = 3, Color = fromRGB(0, 0, 0)}
-]]
+local label = teammates:Label("fart")
+label:Set("hi")
+
+local tab2 = window:Tab("test")
+local f = tab2:SubTab("hi")
+local g = tab2:SubTab("testf")
+local r = f:Section{name = "fart"}
+r:Button{}
+
+local r = g:Section{name = "poop"}
+r:Button{}
+
+library:Init()
+--wait(10)
+--library:Unload()
+library:Notify{duration = 3, Color = fromRGB(0, 0, 0)}
+task.wait(1)
+
+library:Notify{duration = 3, Color = fromRGB(0, 0, 0)}
+]]--
 
 utility.format(library, true)
 return library;
