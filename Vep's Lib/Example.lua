f0191e9f1c9d1686d9af3802a79fec303fef6486
Duckyy <95024755+GhostDuckyy@users.noreply.github.com>
2023-07-10 18:40:00 +0800

    Create Example.lua

---
+++ Vep's Lib/Example.lua
@@ -0,0 +1,32 @@
+local VLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/vep1032/VepStuff/main/VL"))()
+local w = VLib:Window("VEP HUB", "GAMENAME", "G")
+local tab = w:Tab("Tab 1")
+w:Tab("Tab 2")
+
+tab:Button("Button",function()
+  print("Pressed!")
+end)
+
+tab:Toggle("Toggle",function(t)
+  print(t)
+end)
+
+
+tab:Slider("Slider",0,100,70,function(t)
+  print(t)
+end)
+
+
+tab:Dropdown("Dropdown",{"Option 1", "Option 2", "Option 3", "Option 4", "Option 5"},function(t)
+  print(t)
+end)
+
+tab:Colorpicker("Colorpicker",Color3.fromRGB(255, 1, 1),function(t)
+  print(t)
+end)
+
+ss:Textbox("Textbox", true,function(t)
+  print(t)
+end)
+
+tab:Label("Label")
