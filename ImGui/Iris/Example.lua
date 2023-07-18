1bf5c74f0312f5eb953f7e921cac3fed674d1f92
Duckyy <95024755+GhostDuckyy@users.noreply.github.com>
2023-07-18 14:56:21 +0800

    Update Example.lua

--- ImGui/Iris/Example.lua
+++ ImGui/Iris/Example.lua
@@ -1,12 +1,2 @@
-local Iris = loadstring(game:HttpGet("https://raw.githubusercontent.com/x0581/Iris-Exploit-Bundle/main/bundle.lua"))().Init(game.CoreGui)
+local Iris = loadstring(game:HttpGet("https://raw.githubusercontent.com/x0581/Iris-Exploit-Bundle/2.0.4/bundle.lua"))().Init(game.CoreGui)
 Iris:Connect(Iris.ShowDemoWindow)
-Iris:Connect(function()
-   Iris.Window({"My First Window!"}) do
-       Iris.Text({"Hello, World"})
-       if Iris.Button({"Save"}).clicked then
-           print("Clicked!")
-       end
-       Iris.InputNum({"Input"})
-       Iris.End()
-   end
-end)
